28a3a18ae24380c2e962cd4aee905bfc3f04ce5d
Yuri <104217105+Yurizh0101@users.noreply.github.com>
2022-06-28 20:49:28 -0300

    Add files via upload
    Outro Python com as mesmas razões.

--- index.py
+++ index.py
@@ -1,18 +1,24 @@
-x = "Me beija e"
-y = " me amarra no"
-z = " chão."
-print(len(z))
-a = x + y + z
-if "beija" in a:
- print("Boa Vitinho")
-b = "My family is beatiful"
-if "family" in b:
- print(b.upper())
-x = "Hello Zawardo!"
-print(x[0:5])
-listinhaSagaz = ["Mãe","Irmão","Amigos"]
-listinhaSagazINg = ["Mother","Brother","Friends"]
-listinhaSagaz.extend(listinhaSagazINg)
-print(listinhaSagaz)
-for C in listinhaSagazINg:
- print(C)
+x = ("Victor", "Yuri","Carla")
+z = list(x)
+z.append("Igor")
+z.remove("Carla")
+w = tuple(z)
+print(len(w))
+print(w)
+y = (123, 456, 789, 910, 911, 912)
+if 123 in y:
+ print("Yes, that item has in 'y'.") 
+
+A,B = 4,5
+C = A+B/2
+print("Sua média é " + str(C))
+if C >= 7:
+    print("Congratulations, You Are Gay!")
+    
+
+
+ 
+
+
+
+
