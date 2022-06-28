85245ed128bf4f471f1897f97ffab14363d7c050
Yuri <104217105+Yurizh0101@users.noreply.github.com>
2022-06-28 20:45:46 -0300

    Add files via upload
    Python né, to fazendo engenharia de software e estou péssimo.

---
+++ index.py
@@ -0,0 +1,18 @@
+x = "Me beija e"
+y = " me amarra no"
+z = " chão."
+print(len(z))
+a = x + y + z
+if "beija" in a:
+ print("Boa Vitinho")
+b = "My family is beatiful"
+if "family" in b:
+ print(b.upper())
+x = "Hello Zawardo!"
+print(x[0:5])
+listinhaSagaz = ["Mãe","Irmão","Amigos"]
+listinhaSagazINg = ["Mother","Brother","Friends"]
+listinhaSagaz.extend(listinhaSagazINg)
+print(listinhaSagaz)
+for C in listinhaSagazINg:
+ print(C)
