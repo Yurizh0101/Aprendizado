7de8d731d8313756a5a57cbaf025deb5d1078b9d
Yuri <104217105+Yurizh0101@users.noreply.github.com>
2022-07-02 22:13:19 -0300

    Add files via upload
    Treinamentos de condições com o Python, mas acho que é muito grande o uso de código, suspeito que tem forma de minimizar isto!

--- index2.py
+++ index2.py
@@ -1,19 +1,15 @@
-x = ("Victor", "Yuri","Carla")
-z = list(x)
-z.append("Igor")
-z.remove("Carla")
-w = tuple(z)
-print(len(w))
-print(w)
-y = (123, 456, 789, 910, 911, 912)
-if 123 in y:
- print("Yes, that item has in 'y'.") 
-
-A,B = 4,5
-C = A+B/2
+print("Nota da sua Primeira prova:")
+A = int(input())
+print("Nota da sua segunda prova:")
+B = int(input())
+C = (A+B)/2
 print("Sua média é " + str(C))
 if C >= 7:
-    print("Congratulations, You Are Gay!")
+    print("Congratulations, You Are Welcome!")
+else : 
+    if C < 7:
+        print("Sorry, Try Again!")
+
     
 
 
---
+++ index3.py
@@ -0,0 +1,10 @@
+print("Insira seu último salário recebido:")
+A = int(input())
+if A > 2000:
+ print("Seu salário é maior que a média brasileira!")
+else:
+ if A < 2000:
+    print("Seu salário é abaixo da média brasileira")
+ else:
+    print("Seu salário está na média brasileira!")
+    
